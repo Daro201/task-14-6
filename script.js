1930aebfe491e6ead03f862ebd234bf0ea916511
Adrian Meller <melleradria@gmail.com>
2018-07-25 17:49:49 +0200

    Change in script.js

--- script.js
+++ script.js
@@ -57,76 +57,12 @@ var Counter = React.createClass({
 });
 
  
-var element = React.createElement(Counter);
+var element = React.createElement('div', {},
+    React.createElement(Counter),
+    React.createElement(Counter),
+    React.createElement(Counter)
+);
+
 ReactDOM.render(element, document.getElementById('app'));
-
-
-//function multiplying and dividing the counter 
-var Multiplyer = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 1
-        };
-    },
-
-    multiply: function() {
-        this.setState({
-            counter: this.state.counter * 2
-        });
-    },
-
-    divide: function() {
-        this.setState({
-            counter: this.state.counter / 2
-        });
-    },
-
-    render: function() {
-        return React.createElement('div', {},
-            React.createElement('button',{onClick: this.divide}, '/'),
-            React.createElement('span', {}, 'Licznik ' + this.state.counter),
-            React.createElement('button',{onClick: this.multiply}, '*'),
-        );
-    }
-});
-
-
-var element2 = React.createElement(Multiplyer);
-ReactDOM.render(element2, document.getElementById('app2'));
-
-
-
-//function counting sinus and cosinus of the counter
-var SinCosine = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
-    sinus: function() {
-        this.setState({
-            counter: Math.sin(this.state.counter)
-        });
-    },
-
-    cosinus: function() {
-        this.setState({
-            counter: Math.cos(this.state.counter)
-        });
-    },
-
-    render: function() {
-        return React.createElement('div', {},
-            React.createElement('button',{onClick: this.sinus}, 'sin'),
-            React.createElement('span', {}, 'Licznik ' + this.state.counter),
-            React.createElement('button',{onClick: this.cosinus}, 'cos'),
-        );
-    }
-});
-
-
-var element3 = React.createElement(SinCosine);
-ReactDOM.render(element3, document.getElementById('app3'));
 
 
